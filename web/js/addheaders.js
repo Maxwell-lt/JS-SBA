227c02ee4a4ec6abc7c4e820e40e7f0e01d2b072
Maxwell Lanzkron-Tamarazo <maxwell.lt@live.com>
2019-12-10 12:16:58 -0500

    Added index page and implemented first draft of graphing calculator
    TODO: major improvements to graphing calculator algorithm

---
+++ web/js/addheaders.js
@@ -0,0 +1,10 @@
+document.getElementById("navbar").outerHTML = '<nav class="navbar navbar-expand-lg navbar-light bg-light">'+
+	'<a class="navbar-brand" href="./index.html">Home</a>'+
+	'<div class="collapse navbar-collapse" id="navbarNavAltMarkup">'+
+	'<div class="navbar-nav">'+
+	'<a class="nav-item nav-link" href="./graph.html">Graph Generator</a>'+
+	'<a class="nav-item nav-link" href="#">Scientific Calculator</a>'+
+	'<a class="nav-item nav-link" href="#">Unit Conversion</a>'+
+	'</div>'+
+	'</div>'+
+	'</nav>';
---
+++ web/js/drawgraph.js
@@ -0,0 +1,20 @@
+var element = document.getElementById("graphForm");
+element.addEventListener("submit", drawGraph, true);
+
+function drawGraph(e) {
+	e.preventDefault();
+	if (!e.target[0].value || !e.target[1].value || !e.target[2].value) {
+		return;
+	}
+	let A = Number(e.target[0].value);
+	let B = Number(e.target[1].value);
+	let C = Number(e.target[2].value);
+	let points = []
+	for (var x = -10; x <= 10; x++) {
+		let y = (A * x * x) + (B * x) + C;
+		points.push([-(x*5 - 250),-(y*5 - 250)]);
+	}
+	let polyline = points.map(x => x.join(",")).join(" ");
+	console.log(polyline);
+	document.getElementById("graphline").setAttribute("points", polyline);
+}
